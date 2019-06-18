c1445f86335f931b905a4428787b6ba2171d0129
hzzhaoxiangtao@corp.netease.com <hzzhaoxiangtao@corp.netease.com>
2019-06-18 20:46:24 +0800

    feat(): test eslint

--- src/test.js
+++ src/test.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const Button = () => <button>btn</button>;
+
+class Label extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <label>label</label>;
+    }
+}
+
+let test = 'test';
+
+const lambda = x => x;
 
 export default Button;
